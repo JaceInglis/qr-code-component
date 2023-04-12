4c1d98deee0e92586bf5aa311675d0bfaa109d85
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-04-12 14:42:34 -0700

    Write content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      HI
+      <div className='card'>
+        <img className="qr-code" src="images/image-qr-code.png" alt="QR code" />
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
     </div>
   );
 }
